d86c91ad8eecfad30dc2eb6d720347a10507dabe
agambondan <agamgans89@gmail.com>
2022-10-19 10:00:38 +0700

    add hackerrank implements easy happy ladybugs

---
+++ hackkerrank/practice/implements/easy/happy_ladybugs.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+/*
+ * Complete the 'happyLadybugs' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING b as parameter.
+ */
+
+// happyLadybugs
+func happyLadybugs(b string) string {
+	// Write your code here
+	var temp []string
+	for i := 0; i < len(b); i++ {
+		s := string(b[i])
+		if s != "_" {
+			count := strings.Count(b, s)
+			if count < 2 {
+				return "NO"
+			} else {
+				for j := 0; j < count; j++ {
+					temp = append(temp, s)
+				}
+			}
+		}
+	}
+	if strings.Count(b, "_") > 0 {
+		return "YES"
+	} else {
+		var pair int
+		for i := 0; i < len(b)-1; i++ {
+			if b[i] == b[i+1] {
+				pair += 1
+			} else if pair > 0 {
+				pair = 0
+			} else {
+				return "NO"
+			}
+		}
+	}
+	return "YES"
+}
+
+func main() {
+	T := []string{"AABBC", "AABBC_C", "_", "DD__FQ_QQF", "AABCBC"}
+	for i := 0; i < len(T); i++ {
+		fmt.Println(happyLadybugs(T[i]))
+	}
+	fmt.Println()
+	T = []string{"RBY_YBR", "X_Y__X", "__", "B_RRBR"}
+	for i := 0; i < len(T); i++ {
+		fmt.Println(happyLadybugs(T[i]))
+	}
+	fmt.Println()
+	T = []string{"_", "RBRB", "RRRR", "BBB", "BBB_"}
+	for i := 0; i < len(T); i++ {
+		fmt.Println(happyLadybugs(T[i]))
+	}
+	fmt.Println()
+	T = []string{"G", "GR", "_GR_", "_R_G_", "R_R_R", "RRGGBBXX", "RRGGBBXY"}
+	//T = []string{"RRGGBBXX"}
+	for i := 0; i < len(T); i++ {
+		fmt.Println(happyLadybugs(T[i]))
+	}
+	fmt.Println()
+	T = []string{"_FWYSSENEDBO_KSEVUAB_WZ_GASASVEVS_O_NSVBYFNADE_WWVSBKAE_F_VAS_F_AAAEWBE_WEAEOAYV", "ZBF_MIFUXJNQGQRFZVRQUFFFFNGFIBJ_XZVIRFGMJRJFVMNJMF",
+		"YFCA_NXMGJYYGCMMGGGXYNAMFNJJX_", "CBLJUKUWSTIIUKUBQSITSULTJKCUSKBCKB", "A_TOJRPRW__JOJP__WAJT",
+	}
+	for i := 0; i < len(T); i++ {
+		fmt.Println(happyLadybugs(T[i]))
+	}
+
+}
+
+//// happyLadybugs1
+//func happyLadybugs1(b string) string {
+//	if strings.Count(b, string(b[0])) < 1 && strings.Count(b, string(b[0])) == len(b) {
+//		return "YES"
+//	}
+//	for i := 0; i < strings.Count(b, "_"); i++ {
+//		temp = append(temp, "_")
+//	}
+//	for i := 0; i < len(b); i++ {
+//		if string(b[i]) != "_" && strings.Count(b, string(b[i])) < 2 {
+//			return "NO"
+//		}
+//	}
+//	if !strings.Contains(b, "_") {
+//		for i := 1; i < len(b); i += 2 {
+//			if string(b[i-1]) != string(b[i]) {
+//				return "NO"
+//			}
+//		}
+//	}
+//	return "YES"
+//}
--- lib/converter.go
+++ lib/converter.go
@@ -6,6 +6,7 @@ import (
 	"unsafe"
 )
 
+// go 1.18 or higher
 func MapValueToArray[M ~map[K]V, K comparable, V any](m M) []V {
 	r := make([]V, 0, len(m))
 	for _, v := range m {
@@ -14,6 +15,7 @@ func MapValueToArray[M ~map[K]V, K comparable, V any](m M) []V {
 	return r
 }
 
+// go 1.18 or higher
 func MapKeyToArray[M ~map[K]V, K comparable, V any](m M) []K {
 	r := make([]K, 0, len(m))
 	for k := range m {
